59dc37b98a200cb2ca75156603637de29e3e1f94
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2025-09-24 21:40:06 +0100

    Update dashboard.php

--- views/dashboard.php
+++ views/dashboard.php
@@ -170,7 +170,7 @@
                 </select>
             </div>
             
-            <button type="submit" name="afficher" class="px-4 py-2 bg-blue-500 text-white hover:bg-yellow-400 font-medium rounded-lg transition-colors shadow-md flex items-center">
+            <button type="submit" name="afficher" class="self-center px-4 py-2 bg-blue-500 text-white hover:bg-yellow-400 font-medium rounded-lg transition-colors shadow-md flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
                 </svg>
